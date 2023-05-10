e5062734f2bd746926de4944f20311c7faa5255c
Souhayl23 <souhayl.sedira23@gmail.com>
2023-05-10 21:52:12 +0200

    hhj

--- src/components/MenuLogo.vue
+++ src/components/MenuLogo.vue
@@ -1,8 +1,7 @@
 <template>
-<svg width="40" height="21" viewBox="0 0 40 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0 1H40" stroke="#2300FB" stroke-width="2.5" stroke-linejoin="bevel"/>
-<path d="M7 10H40" stroke="#2300FB" stroke-width="2.5" stroke-linejoin="bevel"/>
-<path d="M14 20H40" stroke="#2300FB" stroke-width="2.5" stroke-linejoin="bevel"/>
+
+<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M5 8H13.75M5 12H19M10.25 16L19 16" stroke="#FAEBD7" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 
 </template>
