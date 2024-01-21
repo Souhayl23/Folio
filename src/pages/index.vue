49798219ae4f9a2beea87083362eff7471f53549
Souhayl23 <95019311+Souhayl23@users.noreply.github.com>
2024-01-21 03:04:11 +0100

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -262,21 +262,6 @@ tl.fromTo(".big-text2", { opacity: 0 }, { opacity: 1, duration: 1 });
               </RouterLink>
 								<div class="divider"></div>
 
-                <RouterLink to="mmiscp"> 
-								<div class="menu__item">
-									<a class="menu__item-link clickable">/ MMISCP</a>
-									<img class="menu__item-img" src="../assets/images/projet/MMISCPMINI.svg" />
-									<div class="marquee">
-										<div class="marquee__inner">
-											<span>MMISCP</span>
-											<span>MMISCP</span>
-											<span>MMISCP</span>
-											<span>MMISCP</span>
-										</div>
-									</div>
-								</div></RouterLink>
-
-								<div class="divider"></div>
 
                 <RouterLink to="/national">
                 <div class="menu__item">
@@ -294,39 +279,6 @@ tl.fromTo(".big-text2", { opacity: 0 }, { opacity: 1, duration: 1 });
               </RouterLink>
 
                 <div class="divider"></div>
-                <RouterLink to="streamer"> 
-								<div class="menu__item">
-									<a class="menu__item-link clickable">/ Streamer Goal</a>
-									<img class="menu__item-img" src="../assets/images/projet/STREAMERMINISVG.svg" />
-									<div class="marquee">
-										<div class="marquee__inner">
-											<span>Streamer Goal</span>
-											<span>Streamer Goal</span>
-											<span>Streamer Goal</span>
-											<span>Streamer Goal</span>
-										</div>
-									</div>
-								</div>
-              </RouterLink>
-
-								<div class="divider"></div>
-                <RouterLink to="femme"> 
-								<div class="menu__item">
-									<a class="menu__item-link clickable">/ Parole d'une femme</a>
-									<img class="menu__item-img" src="../assets/images/projet/PAROLEMINI.png" />
-									<div class="marquee">
-										<div class="marquee__inner">
-											<span>Parole d'une femme</span>
-											<span>Parole d'une femme</span>
-											<span>Parole d'une femme</span>
-											<span>Parole d'une femme</span>
-										</div>
-									</div>
-								</div></RouterLink>
-
-								<div class="divider"></div>
-
-
 
 							</nav>
 						</div>
