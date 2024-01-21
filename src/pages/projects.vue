f8a1a0a1a307566e0ef17d25d0e02bd719eba2d3
Souhayl23 <95019311+Souhayl23@users.noreply.github.com>
2024-01-21 22:33:16 +0100

    Update projects.vue

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -124,7 +124,7 @@ export default {
 						</div>
 					</div>
 
-				</section> </div>
+				</section> </div> </section>
     <a
       href="#top"
       class="fixed bottom-20 right-[5vw] z-10 flex h-16 w-20 items-center justify-center bg-[#2300FB] text-xl font-normal text-white opacity-50 transition-all hover:h-16 hover:w-24 hover:opacity-100"
